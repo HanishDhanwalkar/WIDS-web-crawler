cdc96535461c997bf1dd36f5a408cb458d7acd10
HanishDhanwalkar <hanishpd1729@gmail.com>
2024-01-18 21:50:29 +0530

    added bookcat spider

---
+++ bookcatwise/bookcatwise/spiders/bookcatwise.py
@@ -0,0 +1,27 @@
+import scrapy
+import pandas as pd
+
+
+class BookSpider(scrapy.Spider):
+    name = "bookcat"
+
+    print("Scraping Started")
+
+    def start_requests(self):
+
+        urls = ["https://books.toscrape.com/index.html"]
+
+        for url in urls:
+            yield scrapy.Request(url=url, callback=self.parse)
+            
+
+    def parse(self, response):        
+        cats  = []
+
+        for cat in response.css('li a::text').getall():
+            cats.append(cat.strip())
+
+        print("bbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbb:",cats)
+
+
+        
